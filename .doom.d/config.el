3328eee299542748ff4c60e5ba9112fd578c60ba
Paul Haider <paolo.haider@gmail.com>
2019-10-18 11:11:36 +0200

    set doom theme

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,3 +1,16 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+
+(require 'doom-themes)
+
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; may have their own settings.
+(load-theme 'doom-one t)
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
