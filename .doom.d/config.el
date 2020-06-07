3348040d07f001ca70505eb8b4f9c8e2396a47ca
Paul Haider <paolo.haider@gmail.com>
2020-06-07 12:52:36 +0200

    configure deft in doom

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -16,7 +16,7 @@
 (doom-themes-org-config)
 
 ;; Load the font
-(setq doom-font (font-spec :family "Fira Code Retina" :size 15))
+(setq doom-font (font-spec :family "Fira Code" :size 16))
 
 ;; useful tools for getting bibtex entries and pdf files of journal manuscripts
 (require 'doi-utils)
@@ -52,3 +52,11 @@
                ("\\subsubsubsection{%s}" . "\\subsubsubsection*{%s}")))
 (setq org-latex-listings t)
 (add-to-list 'org-latex-packages-alist '("" "listingsutf8"))
+
+(use-package deft
+      :after org
+      :custom
+      (deft-recursive t) ; search subdirectories
+      (deft-use-filter-string-for-filename t)
+      (deft-default-extension "org")
+      (deft-directory "~/org/"))
