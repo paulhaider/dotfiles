57a5017fa07d0439d965feb4bd98efe68692dc8b
Paul Haider <paolo.haider@gmail.com>
2022-06-24 16:56:36 +0200

    update aare script for waybar

--- .config/waybar/aare.py
+++ .config/waybar/aare.py
@@ -1,38 +1,18 @@
 #!/usr/bin/env python
 
-import re
-import os
 import json
-import requests
-from datetime import datetime
+from pyaare.pyaare import PyAare
 
-os.system("")
+city = "Bern"
 
 data = {}
 
-# weather = requests.get()  # .json()
-
-import urllib.request
-
-def escape_ansi(line):
-    ansi_escape = re.compile(r'(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]')
-    return ansi_escape.sub('', line)
-
-url = "https://aare.li/"
-
-uf = urllib.request.urlopen(url)
-html = uf.read()
-string = str(html)
-temp = string[string.find('Wasser') + 25:string.find('Wasser') + 29]
-data['text'] = "Aare " + temp + "°"
-data['tooltip'] = escape_ansi(html.decode())
-
-# print(html.decode())
-
-# data['text'] = weather['nearest_area'][0]['areaName'][0]['value'] + \
-#         " "+weather['current_condition'][0]['FeelsLikeC']+"°" + \
-#         " "+WEATHER_CODES[weather['current_condition'][0]['weatherCode']]
-
-# data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
+aare = PyAare(city=city)
+data['text'] = "Aare " + str(aare.tempC) + "°"
+data['tooltip'] = f"<b>Aaretemperatur z'Bärn</b>\n"
+#  data['tooltip'] += f"Ds letschte öpdeit hets vor {aare.retrievedAt} gä\n"
+data['tooltip'] += f"Temperatur: {aare.tempC}\u00b0C  -  {aare.tempText}\n"
+data['tooltip'] += f"Wassermängi: {aare.flow} m³/s  -  {aare.flowText}\n"
+data['tooltip'] += f"Das Wasser i ca. 2 Stung: {aare.tempC2h}\u00b0C  -  {aare.tempC2hText}"
 
 print(json.dumps(data))
