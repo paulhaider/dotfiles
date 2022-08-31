58d38a690752f3156a242c954453d1eabf89eb95
Paul Haider <paolo.haider@gmail.com>
2022-08-31 14:56:16 +0200

    configure org roam ui in doom

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -109,22 +109,17 @@
          :target (file+head "%<%Y%m%d>.org"
                             "#+title: %<%A, %x>\n"))))
 
-(use-package org-roam-bibtex
-  :after org-roam
-  :hook (org-roam-mode . org-roam-bibtex-mode)
-  :config
+(use-package! websocket
+    :after org-roam)
 
-  (setq orb-preformat-keywords
-        '("citekey" "title" "url" "author-or-editor" "keywords" "file")
-        orb-process-file-keyword t
-        orb-file-field-extensions '("pdf"))
-
-  (setq org-roam-capture-templates
-        '(("d" "default" plain "%?"
-           :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                              "#+title: ${title}\n")
-           :unnarrowed t)
-          ("r" "bibliography reference" plain
-           (file "/home/paul/Templates/noter_template.org")
-           :target
-           (file+head "notes/${citekey}.org" "#+title: ${title}\n")))))
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,6 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
+;; When using org-roam via the `+roam` flag
+(unpin! org-roam)
+(package! org-roam-ui)
+
 ;; modern style for org buffers
 (package! org-modern :recipe (:host github :repo "minad/org-modern"))
 
