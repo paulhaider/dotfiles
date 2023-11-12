edc11b2248eccbdcf9e6a7c10a7147aa202f7cf3
Paul Haider <paul.haider@unibe.ch>
2023-11-12 18:03:43 +0100

    improve doom config with GTD agenda
    add config.el too

---
+++ .config/doom/config.el
@@ -0,0 +1,214 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Paul Haider"
+      user-mail-address "paul.haider@pm.me")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+
+;; (setq doom-font (font-spec :family "Fira Code" :size 14 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 15))
+
+;; (setq doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 14))
+;; (setq doom-unicode-font (font-spec :family "FiraCode Nerd Font Mono" :size 14))
+;; (setq doom-variable-pitch-font (font-spec :family "Fira Sans" :size 15))
+
+(setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 14))
+(setq doom-unicode-font (font-spec :family "JetBrains Mono Nerd Font" :size 14))
+(setq doom-variable-pitch-font (font-spec :family "Fira Sans" :size 15))
+
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+;; (setq doom-theme 'nano-light)
+;; (after! doom-themes
+;;   (load-theme 'doom-nano-light t))
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+
+(setq! zot_bib (concat (getenv "HOME") "/Documents/braindump/zotLib.bib")
+       org_directory (concat (getenv "HOME") "/Documents/braindump/")
+       org-directory org_directory
+       ;; org_gtd (concat (getenv "HOME") "/Documents/braindump/")
+       org_notes (list org_directory)
+       org_journal (concat (getenv "HOME") "/Documents/braindump/journal/") ; for some reason I do not understand agenda view only works when I name the folder daily
+       org_areas (directory-files-recursively (file-name-concat org-directory "areas") "\\.org$")
+       org-agenda-files (append org_areas (list "inbox.org" "agenda.org" "notes.org" "private.org" "reading.org")))
+
+(setq! org-journal-dir org_journal
+       org-journal-file-type 'daily
+       org-journal-file-format "%Y%m%d.org"
+       org-journal-date-format "%A, %x"
+       org-journal-date-prefix "#+title: "
+       org-journal-time-prefix "* "
+       org-journal-carryover-delete-empty-journal 'ask
+       org-journal-enable-agenda-integration nil)  ; show current journal todos in agenda
+
+(setq! citar-bibliography '("~/Documents/braindump/zotLib.bib"))
+(setq! citar-notes-paths '("~/Documents/braindump/references/"))
+
+(setq! org-roam-directory org_directory
+       org-roam-dailies-directory org_journal
+       org-roam-dailies-capture-templates
+       '(("d" "default" entry
+          "* %?"
+          :target (file+head "%<%Y%m%d>.org"
+                             "#+title: %<%A, %x>\n"))))
+
+(add-hook! 'org-mode-hook 'org-fragtog-mode)
+(add-hook! 'org-mode-hook 'org-appear-mode)
+;; enable org-modern-mode
+(add-hook! 'org-mode-hook #'org-modern-mode)
+(add-hook! 'org-agenda-finalize-hook #'org-modern-agenda)
+;; (with-eval-after-load 'org (global-org-modern-mode))
+
+(after! org
+  (setq! org-log-done t)
+  (plist-put org-format-latex-options :scale 1.0)
+  (setq! org-todo-keywords
+         '((sequence "TODO(t)" "NEXT(n)" "HOLD(h)" "|" "DONE(d)"))))
+
+;; refile
+(setq! org-refile-use-outline-path 'file)
+(setq! org-outline-path-complete-in-steps nil)
+;; (setq! org-refile-targets '((org-agenda-files :maxlevel . 3)))
+(setq! org-refile-targets '((org-agenda-files :regexp . "\\(?:\\(?:Note\\|Task\\)s\\)")))
+
+;; TODO
+(defun log-todo-next-creation-date (&rest ignore)
+  "Log NEXT creation time in the property drawer under the key 'ACTIVATED'"
+  (when (and (string= (org-get-todo-state) "NEXT")
+             (not (org-entry-get nil "ACTIVATED")))
+    (org-entry-put nil "ACTIVATED" (format-time-string "[%Y-%m-%d]"))))
+(add-hook 'org-after-todo-state-change-hook #'log-todo-next-creation-date)
+
+;; configure org-capture with custom templates based on https://www.labri.fr/perso/nrougier/GTD/index.html
+(after! org-capture
+  (setq! +org-capture-todo-file "inbox.org")
+  (setq! org-capture-templates
+         `(("i" "Inbox" entry (file +org-capture-todo-file)
+            ,(concat "* TODO %?\n"
+                     "Entered on %U"))
+           ("m" "Meeting" entry  (file+headline "agenda.org" "Future")
+            ,(concat "* %? :meeting:\n"
+                     "<%<%Y-%m-%d %a %H:00>>"))
+           ;; ("@" "Inbox [mu4e]" entry (file "inbox.org")
+           ;;  ,(concat "* TODO Reply to \"%a\" %?\n"
+           ;;           "Entered on %U"))
+           ("n" "Note" entry  (file "notes.org")
+            ,(concat "* Note (%a)\n"
+                     "Entered on %U\n" "\n" "%?")))))
+
+;; Agenda
+(setq! org-agenda-custom-commands
+      '(("g" "Get Things Done (GTD)"
+         ((agenda ""
+                  ((org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'deadline))
+                   (org-deadline-warning-days 0)))
+          (todo "NEXT"
+                ((org-agenda-skip-function
+                  '(org-agenda-skip-entry-if 'deadline))
+                 (org-agenda-prefix-format "  %i %-12:c [%e] ")
+                 (org-agenda-overriding-header "\nTasks\n")))
+          (agenda nil
+                  ((org-agenda-entry-types '(:deadline))
+                   (org-agenda-format-date "")
+                   (org-deadline-warning-days 7)
+                   (org-agenda-skip-function
+                    '(org-agenda-skip-entry-if 'notregexp "\\* NEXT"))
+                   (org-agenda-overriding-header "\nDeadlines")))
+          (tags-todo "inbox"
+                     ((org-agenda-prefix-format "  %?-12t% s")
+                      (org-agenda-overriding-header "\nInbox\n")))
+          (tags "CLOSED>=\"<today>\""
+                ((org-agenda-overriding-header "\nCompleted today\n")))))))
+
+;; Use full window for org-capture
+(add-hook! 'org-capture-mode-hook 'delete-other-windows)
+
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+
+;; (setq org-gtd-update-ack "3.0.0")
+;; (use-package! org-gtd
+;;   :after org
+;;   :config
+;;   (setq org-gtd-directory org_gtd)
+;;   (setq org-edna-use-inheritance t)
+;;   (org-edna-mode)
+;;   (map! :leader
+;;         (:prefix ("d" . "org-gtd")
+;;          :desc "Capture"        "c"  #'org-gtd-capture
+;;          :desc "Engage"         "e"  #'org-gtd-engage
+;;          :desc "Process inbox"  "p"  #'org-gtd-process-inbox
+;;          :desc "Show all next"  "n"  #'org-gtd-show-all-next
+;;          :desc "Stuck projects" "s"  #'org-gtd-review-stuck-projects))
+;;   (map! :map org-gtd-clarify-map
+;;         :desc "Organize this item" "C-c c" #'org-gtd-organize))
+
+;; accept completion from copilot and fallback to company
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
+         ("C-<tab>" . 'copilot-accept-completion-by-word)
+         :map copilot-completion-map
+         ("<tab>" . 'copilot-accept-completion)
+         ("TAB" . 'copilot-accept-completion)))
+
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -4,8 +4,8 @@
 ;; in. Remember to run 'doom sync' after modifying it!
 
 ;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;      documentation. There you'll find a "Module Index" link where you'll find
-;;      a comprehensive list of Doom's modules and what flags they support.
+;;      documentation. There you'll find a link to Doom's Module Index where all
+;;      of our modules are listed, including what flags they support.
 
 ;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
 ;;      'C-c c k' for non-vim users) to view its documentation. This works on
@@ -15,6 +15,9 @@
 ;;      directory (for easy access to its source code).
 
 (doom! :input
+       ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
+       ;;chinese
+       ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
@@ -28,22 +31,22 @@
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       ;;(emoji +unicode)  ; 🙂
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       (emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       indent-guides     ; highlighted indent columns
+       ;;indent-guides     ; highlighted indent columns
        ligatures         ; ligatures and symbols to make your code pretty again
-       minimap           ; show a map of the code on the side
+       ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       ;;tabs              ; a tab bar for Emacs
+       tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
-       (vc-gutter +pretty +diff-hl)  ; vcs diff in the fringe
+       ;;unicode           ; extended unicode support for various languages
+       (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
@@ -102,6 +105,7 @@
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -111,7 +115,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       ;;cc                ; C > C++ == 1
+       ;;(cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -132,6 +136,7 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
+       ;;(graphql +lsp)    ; Give queries a REST
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
@@ -148,7 +153,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +journal +present +pretty +roam2)  ; organize your plain life in plain text
+       (org +journal +pomodoro +pretty +roam2)  ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -159,13 +164,13 @@
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
+       ;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
        ;;yaml              ; JSON, but readable
